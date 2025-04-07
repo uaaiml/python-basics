16f0aaddd5e4dab6dcba516f6a490f214b53d3c4
uaaiml <uaaimldl@gmail.com>
2025-04-07 08:07:54 +0530

    Add files via upload

---
+++ 01_basic/ex02_handle_variables.py
@@ -0,0 +1,16 @@
+
+# Define & print the integer variable
+num1 = 1000
+print("Integer value is:", num1)
+
+# Define & print the decimal variable
+num2 = 100.5
+print("Decimal value is:", num2)
+
+# Define & print the string variable
+name = "John"
+print("String value is:", name)
+
+# Define & print the boolean variable
+flag = True
+print("Boolean value is:", flag)
