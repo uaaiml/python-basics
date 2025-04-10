bcd848454b302ce9bafd39e877ee61f0a3a98652
uaaiml <uaaimldl@gmail.com>
2025-04-10 08:56:11 +0530

    Add files via upload

---
+++ 01_basic/ex03_handle_datatype.py
@@ -0,0 +1,18 @@
+
+# Assign an integer value to the variable num
+num = 1000
+
+# Print the data type of num
+print(type(num))
+
+# Assign a string value to the variable name
+name = "John"
+
+# Print the data type of name
+print(type(name))
+
+# Convert num to a floating-point number and print it
+print(float(num))
+
+# Convert num to a boolean and print it
+print(bool(num))
