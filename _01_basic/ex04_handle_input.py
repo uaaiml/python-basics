11c56426f4234707f9874392f7adb05a2553a151
uaaiml <uaaimldl@gmail.com>
2025-04-15 08:32:03 +0530

    Add files via upload

---
+++ _01_basic/ex04_handle_input.py
@@ -0,0 +1,8 @@
+# Take string input data from user
+string_value = input("Enter String Value = ")
+print("Entered String Value is = " , string_value)
+
+# Take integer input data from user
+integer_value = int(input("Enter Integer Number  = "))
+print("Entered Integer Number is = " , integer_value)
+
