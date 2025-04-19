a78a60f8530ab3a21b3444c6fe75c930ad3d0bd7
uaaiml <uaaimldl@gmail.com>
2025-04-19 07:51:20 +0530

    Add files via upload

---
+++ _01_basic/ex05_handle_operators.py
@@ -0,0 +1,29 @@
+num1 = 10
+num2 = 20
+num3 = 30
+num4 = 40
+num5 = 15.0
+greet = "Good Morning"
+
+# Arithmetic Operator
+addition = num1 + num2
+print("Addition:", addition)
+
+# Relational Operator
+print(num1 > num2)
+
+# Logical Operator
+print(num1 > num2 and num2 > num3)
+
+# Assignment Operator
+num4 += 10
+print("Result:", num4)  
+
+# Identity Operator
+print(num1 is num5)
+
+# Membership Operator
+print("Good" in greet)
+
+
+
