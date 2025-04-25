2b0da8cf756b1226bc9ad16de5e4ec420d8a9001
uaaiml <uaaimldl@gmail.com>
2025-04-25 13:57:59 +0530

    Add files via upload

---
+++ _01_basic/ex06_handle_string.py
@@ -0,0 +1,18 @@
+# Formatting string using f-string
+uname = "David"
+age = 25
+print(f"User is {uname} and he is {age} years old.")
+
+# Multi-line string
+multi_line_string = """This is a multi-line string.
+It spans across multiple lines.
+Python makes it easy with triple quotes!"""
+print(multi_line_string)
+
+# Length of the string
+name = "John"
+print(len(name))
+
+# String built-in function
+topic = "  Artificial Intelligence  "
+print(topic.strip())
